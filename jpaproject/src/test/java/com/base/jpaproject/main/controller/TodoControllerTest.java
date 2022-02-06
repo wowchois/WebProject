58fc08c4eae99e238971fe7884129a92e9cca930
wowchois <inschoi57@naver.com>
2022-02-06 23:54:03 +0900

    log4j setting

--- jpaproject/src/test/java/com/base/jpaproject/main/controller/TodoControllerTest.java
+++ jpaproject/src/test/java/com/base/jpaproject/main/controller/TodoControllerTest.java
@@ -4,8 +4,19 @@ import com.base.jpaproject.main.entity.Todo;
 import com.base.jpaproject.main.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@EnableWebMvc
 class TodoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    /*
     @Autowired
     private final TodoRepository todoRepository;
 
@@ -18,6 +29,13 @@ class TodoControllerTest {
         Todo todo = Todo.builder().item("test").build();
 
         this.todoRepository.save(todo);
+    }
+    */
+    @Test
+    void todoWebTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/todo/add"))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 
 }
