d4c8e762f6bf7693b24b984a47a52616a1a57111
wowchois <inschoi57@naver.com>
2022-02-15 23:35:49 +0900

    todo select commit

--- jpaproject/src/main/java/com/base/jpaproject/main/controller/TodoController.java
+++ jpaproject/src/main/java/com/base/jpaproject/main/controller/TodoController.java
@@ -6,8 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -17,10 +21,29 @@ public class TodoController {
 
     private final TodoService todoService;
 
-    @GetMapping("/add")
-    public ResponseEntity<Todo> AddTodo(String item){
+    //추가
+    @PostMapping("/add")
+    public ResponseEntity<?> AddTodo(String item){
         Todo todo = todoService.addTodo(item);
         return ResponseEntity.ok(todo);
+    }
+
+    //전체조회
+    @GetMapping("/getAll")
+    public ResponseEntity<?> GetAllTodo(){
+        List<Todo> list = todoService.getAllTodo();
+        return ResponseEntity.ok(list);
+    }
+
+    //id 1개 조회
+    @GetMapping("/getOne")
+    public ResponseEntity<?> GetTodo(Long id){
+        Optional<Todo> todo = todoService.getTodo(id);
+        if(todo.isPresent()){
+            return ResponseEntity.ok(todo);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
--- jpaproject/src/main/java/com/base/jpaproject/main/service/TodoService.java
+++ jpaproject/src/main/java/com/base/jpaproject/main/service/TodoService.java
@@ -2,8 +2,12 @@ package com.base.jpaproject.main.service;
 
 import com.base.jpaproject.main.entity.Todo;
 import com.base.jpaproject.main.repository.TodoRepository;
+import io.swagger.models.auth.In;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +15,7 @@ public class TodoService {
 
     private final TodoRepository todoRepository;
 
+    //insert
     public Todo addTodo(String item){
         Todo todo = Todo.builder()
                 .item(item)
@@ -19,6 +24,15 @@ public class TodoService {
         todo = todoRepository.save(todo);
 
         return todo;
+    }
+
+    //select all
+    public List<Todo> getAllTodo(){
+        return todoRepository.findAll();
+    }
+
+    public Optional<Todo> getTodo(Long id){
+        return todoRepository.findById(id);
     }
 
 }
